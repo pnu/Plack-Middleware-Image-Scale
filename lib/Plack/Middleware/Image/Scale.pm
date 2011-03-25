8e86f7ae3715b5066ef04cb6e92909bda2650833
Panu Ervamaa <panu.ervamaa@frantic.com>
2011-03-25 17:22:29 +0200

    refactoring the path matching

--- lib/Plack/Middleware/Image/Scale.pm
+++ lib/Plack/Middleware/Image/Scale.pm
@@ -6,23 +6,52 @@ use Image::Scale;
 
 extends 'Plack::Middleware';
 
-sub call {
-    my ($self,$env) = @_;
+has path => (
+    is => 'rw', lazy => 1,
+    isa => 'RegexpRef',
+    default => sub { qr{^(.*)/(.+)_(.+)\.(jpg|png)$} }
+);
 
-    return $self->app->($env) unless 
-        $env->{PATH_INFO} =~ m{^(.*)/(.+)_(.+)\.(jpg|png)$};
+has prop => (
+    is => 'rw', lazy => 1,
+    isa => 'RegexpRef',
+    default => sub { qr{^(\d+)x(\d+)(?:-(\w+))?$} }
+);
 
-    my ($path,$basename,$prop,$ext) = ($1, $2, $3, $4);
-    my ($width,$height) = split 'x', $prop;
+has oext => (
+    is => 'rw', lazy => 1,
+    isa => 'ArrayRef',
+    default => sub { [qw( jpg png gif )] }
+);
+
+sub original {
+    my ($self,$env,$path,$basename) = @_;
 
     my $res;
-    for my $oext ( qw( jpg png gif )) {
+    for my $oext ( @{$self->oext} ) {
         local $env->{PATH_INFO} = "$path/$basename.$oext";
         $res = $self->app->($env);
         last if $res->[0] == 200;
     }
 
-    return $self->app->($env) unless $res->[0] == 200;
+    return $res; 
+}
+
+sub call {
+    my ($self,$env) = @_;
+
+    return $self->app->($env) unless
+        my ($path,$basename,$prop,$ext) =
+        $env->{PATH_INFO} =~ $self->path;
+
+    return $self->app->($env) unless
+        my ($width,$height,$flag) =
+        $prop =~ $self->prop;
+
+    my $res = $self->original($env,$path,$basename);
+
+    return $self->app->($env) unless
+        $res->[0] == 200;
 
     Plack::Util::response_cb( $res, sub {
         my $res = shift;
