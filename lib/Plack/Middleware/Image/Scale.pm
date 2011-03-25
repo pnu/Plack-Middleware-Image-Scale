c50610a992a5c313a28c24bc422296363a886c6e
Panu Ervamaa <panu.ervamaa@frantic.com>
2011-03-25 22:31:41 +0200

    support more size specs, refactoring and added more configuration options

--- lib/Plack/Middleware/Image/Scale.pm
+++ lib/Plack/Middleware/Image/Scale.pm
@@ -1,35 +1,64 @@
 package Plack::Middleware::Image::Scale;
 use Moose;
 use Plack::Util;
+use Plack::MIME;
 use Try::Tiny;
 use Image::Scale;
 
 extends 'Plack::Middleware';
 
-has path => (
-    is => 'rw', lazy => 1,
-    isa => 'RegexpRef',
-    default => sub { qr{^(.*)/(.+)_(.+)\.(jpg|png)$} }
+has match_path => (
+    is => 'rw', lazy => 1, isa => 'RegexpRef',
+    default => sub { qr{^(.*)/(.+)_(.+)\.(jpg|jpeg|png)$} }
 );
 
-has prop => (
-    is => 'rw', lazy => 1,
-    isa => 'RegexpRef',
-    default => sub { qr{^(\d+)x(\d+)(?:-(\w+))?$} }
+has match_spec => (
+    is => 'rw', lazy => 1, isa => 'RegexpRef',
+    default => sub { qr{^(\d*)x(\d*)(?:-(\w+))?$} }
 );
 
-has oext => (
-    is => 'rw', lazy => 1,
-    isa => 'ArrayRef',
+has orig_ext => (
+    is => 'rw', lazy => 1, isa => 'ArrayRef',
     default => sub { [qw( jpg png gif )] }
 );
 
-sub original {
+has memory_limit => (
+    is => 'rw', lazy => 1, isa => 'Int',
+    default => 10_000_000 # bytes
+);
+
+sub call {
+    my ($self,$env) = @_;
+
+    ## Check that uri matches and extract the pieces, or pass thru
+    return $self->app->($env) unless
+        my ($path,$basename,$prop,$ext) =
+        $env->{PATH_INFO} =~ $self->match_path;
+
+    ## Extract image size and options (flag)
+    return $self->app->($env) unless
+        my ($width,$height,$flag) =
+        $prop =~ $self->match_spec;
+
+    my $res = $self->fetch_orig($env,$path,$basename);
+    return $self->app->($env) unless
+        $res->[0] == 200;
+
+    ## Post-process the response with a body filter
+    Plack::Util::response_cb( $res, sub {
+        my $res = shift;
+        my $ct = Plack::MIME->mime_type(".$ext");
+        Plack::Util::header_set( $res->[1], 'Content-Type', $ct );
+        return $self->body_scaler( $width, $height, $flag, $ct );
+    });
+}
+
+sub fetch_orig {
     my ($self,$env,$path,$basename) = @_;
 
     my $res;
-    for my $oext ( @{$self->oext} ) {
-        local $env->{PATH_INFO} = "$path/$basename.$oext";
+    for my $ext ( @{$self->orig_ext} ) {
+        local $env->{PATH_INFO} = "$path/$basename.$ext";
         $res = $self->app->($env);
         last if $res->[0] == 200;
     }
@@ -37,55 +66,58 @@ sub original {
     return $res; 
 }
 
-sub call {
-    my ($self,$env) = @_;
+sub body_scaler {
+    my $self = shift;
+    my @args = @_;
 
-    return $self->app->($env) unless
-        my ($path,$basename,$prop,$ext) =
-        $env->{PATH_INFO} =~ $self->path;
+    my $buffer = q{};
+    my $filter_cb = sub {
+        my $chunk = shift;
 
-    return $self->app->($env) unless
-        my ($width,$height,$flag) =
-        $prop =~ $self->prop;
+        ## Buffer until we get EOF
+        if ( defined $chunk ) {
+            $buffer .= $chunk;
+            return q{}; #empty
+        }
 
-    my $res = $self->original($env,$path,$basename);
+        ## Return EOF when done
+        return if not defined $buffer;
 
-    return $self->app->($env) unless
-        $res->[0] == 200;
+        ## Process the buffer
+        my $img = $self->image_scale(\$buffer,@args);
+        undef $buffer;
+        return $img;
+    };
 
-    Plack::Util::response_cb( $res, sub {
-        my $res = shift;
-        my $buffer;
-        Plack::Util::header_set( $res->[1], 'Content-Type',
-            $ext eq 'jpg' ? 'image/jpeg' :
-            $ext eq 'png' ? 'image/png'  : undef
-        );
-        return sub {
-            my $chunk = shift;
-            if ( defined $chunk ) {
-                $buffer .= $chunk;
-                return q{};
-            } elsif ( defined $buffer ) {
-                try {
-                    my $img = Image::Scale->new(\$buffer);
-                    $img->resize({
-                        width => $width,
-                        height => $height,
-                        keep_aspect => 1,
-                    });
-                    my $scaled =
-                        $ext eq 'jpg' ? $img->as_jpeg :
-                        $ext eq 'png' ? $img->as_png  : undef;
-                    undef $buffer;
-                    return $scaled;
-                } catch {
-                    return;
-                };
-            } else {
-                return;
-            }
-        };
-    });
+    return $filter_cb;
+}
+
+sub image_scale {
+    my ($self, $bufref, $width, $height, $flag, $ct) = @_;
+
+    $width  = 0  unless $width;
+    $height = 0  unless $height;
+    $flag   = '' unless $flag;
+
+    my $output;
+    try {
+        my $img = Image::Scale->new($bufref);
+
+        ## TODO: skip if only converting format..
+        ## Looks like Image::Scale doesn't return data unless we call scale.
+        $img->resize({
+            $width  > 0     ? (width  => $width)  : (),
+            $height > 0     ? (height => $height) : (),
+            $flag eq 'fill' ? (keep_aspect => 1)  : (),
+            memory_limit => $self->memory_limit,
+        });
+
+        $output = $ct eq 'image/jpeg' ? $img->as_jpeg :
+                  $ct eq 'image/png'  ? $img->as_png  :
+                  undef;
+    };
+
+    return $output;
 }
 
 1;
